f1b28cb22b947aaaf3f84cb41b0865e4bbd1965d
Bautista Alvarez Poli <64969775+bautistapoli@users.noreply.github.com>
2020-09-25 10:51:43 -0300

    Add files via upload

--- Programacion/simple-web-page-master/js/Characters.js
+++ Programacion/simple-web-page-master/js/Characters.js
@@ -7,6 +7,9 @@ class Personajes{
   }
   attack(player_2) {
     player_2.health = player_2.health - this.power + player_2.shield
+
+  } spower(player_2){
+    player_2.health = player_2 == Gigant ? player_2.health - this.power : player_2.health - this.power * 2
   }
 }
 
@@ -24,12 +27,11 @@ class Knight extends Personajes{
 
 class Gigant extends Personajes{
   constructor(){
-    super(750,0,60)
+    super(450,0,60)
   }
 }
 
-var player_1 = new Gigant
-var player_2 = new Gigant
+
 var turn = 0
 //if (turno % 2 == 0){
 //   player_1.life = player_1.life - player_2.power
@@ -42,57 +44,52 @@ var turn = 0
 
 console.log("Hello, this game is call Characters")
 
+var player_one = 6
+var player_two = 5
+var characters = [player_one , player_two]
+
+console.log(characters.length);
 
 
+var game = function ([characters]){
+  for(i = 0 ; i < characters.length ; i++){
+    var random_num = Math.round(Math.random() * 3)
+      switch (random_num) {
+        case 1 :
+          characters[i] = new Archer
+          console.log(characters[i]);
+          break;
+        case 2 :
+          characters[i] = new Gigant
+          console.log(characters[i]);
+          break;
+        default:
+          characters[i] = new Knight
+          console.log(characters[i]);
+        }
+      }
+      fight([characters]);
+      function fight([characters]){
+        while (player_one.health > 0 && player_two.health > 0){
+          var random_specs = Math.floor(Math.random() * 10) + 1;
+            console.log(random_specs)
+            //turn = turn == 1 ? 0 : 1
+            if (random_specs >= 9){
+              turn == 0 ? player_one.spower(player_two) : player_two.spower(player_one)
+              console.log("super attack");
+              turn = turn == 1 ? 0 : 1
+            } else {
+              turn == 0 ? player_one.attack(player_two) : player_two.attack(player_one)
+              turn = turn == 1 ? 0 : 1
+            }
+          }
+        // player_two.health = random_specs >= 9 ? player_two.health - player_one.power : player_two.health
+        var winner = player_two.health <= 0 ? "Wins player one " : "Wins player two"
+        console.log(winner); 
+      }
+    }
 
 
 // el primer if es para la equivocacion del ataque
 // el primer else es que no se equivoca
 // despues es el super ataque
-
-var who_wins = function( player_one , player_two){
-  while (player_one.health > 0 && player_two.health > 0){
-    var random_specs = Math.floor(Math.random() * 10) + 1;
-    //console.log(random_specs)
-    if (random_specs <= 2){
-      console.log(turn == 1 ? "Player 2 failed the attack" : "Player 1 failed the attack");
-      turn = turn == 1 ? 0 : 1
-    }else {
-      if(turn == 0){
-        player_one.attack(player_two)
-        turn++
-        // player_two.health = random_specs >= 9 ? player_two.health - player_one.power : player_two.health
-        if (random_specs >= 9){
-          console.log("Super attack to player 2");
-          player_two.health = player_two.health - player_one.power
-        }
-      }else {
-        player_two.attack(player_one)
-        turn = turn - 1
-        if (random_specs >= 9){
-          console.log("Super attack to player 2");
-          player_two.health = player_two.health - player_one.power
-        }
-      }
-    }
-  }
-  var winner = player_two.health <= 0 ? "Wins player one " : "Wins player two"
-  console.log(winner)
-}
-
-// var opponent = function (){
-//   var random_num = Math.round(Math.random() * 3)
-//     switch (random_num) {
-//       case 1 :
-//         var jugador_1 = new Archer
-//         console.log(jugador_1)
-//         break;
-//       case 2 :
-//         var jugador_1 = new Gigant
-//         console.log(jugador_1)
-//         break;
-//       default:
-//         var jugador_1 = new Knight
-//         console.log(jugador_1)
-//       }
-//     }
