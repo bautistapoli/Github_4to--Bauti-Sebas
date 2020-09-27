d18027c51293496f400e2bf64b1d4c663bc6b35c
Bautista Alvarez Poli <64969775+bautistapoli@users.noreply.github.com>
2020-09-26 23:05:56 -0300

    Add files via upload

--- Programacion/simple-web-page-master/js/Characters.js
+++ Programacion/simple-web-page-master/js/Characters.js
@@ -15,7 +15,7 @@ class Personajes{
 
 class Archer extends Personajes{
   constructor(){
-    super(120,15,90)
+    super(120,25,90)
   }
 }
 
@@ -27,7 +27,7 @@ class Knight extends Personajes{
 
 class Gigant extends Personajes{
   constructor(){
-    super(450,0,60)
+    super(450,0,50)
   }
 }
 
@@ -44,52 +44,61 @@ var turn = 0
 
 console.log("Hello, this game is call Characters")
 
-var player_one = 6
-var player_two = 5
+var player_one;
+var player_two;
 var characters = [player_one , player_two]
 
-console.log(characters.length);
 
+var raffle = function (characters){
+  }
 
-var game = function ([characters]){
-  for(i = 0 ; i < characters.length ; i++){
+// turn 0 is for player 1
+
+function fight(player_one, player_two){
+  while (player_one.health > 0 && player_two.health > 0){
+    var random_specs = Math.floor(Math.random() * 10) + 1;
+      if (random_specs >= 9){
+        if (turn == 0){
+          player_one.spower(player_two)
+          console.log("super attack from player one") 
+        } else {
+          player_two.spower(player_one)
+          console.log("super attack from player two")
+        }
+      } else if (random_specs > 2 && random_specs < 9 ) {
+        turn == 0 ? player_one.attack(player_two) : player_two.attack(player_one)
+      } turn = turn == 1 ? 0 : 1
+    }
+  var winner = player_two.health <= 0 ? "Wins player one " : "Wins player two"
+  console.log(winner);
+}
+
+var game = function (characters){
+  for(var i = 0 ; i < 2 ; i++){
     var random_num = Math.round(Math.random() * 3)
       switch (random_num) {
         case 1 :
           characters[i] = new Archer
-          console.log(characters[i]);
+          console.log(characters[i])
           break;
         case 2 :
           characters[i] = new Gigant
-          console.log(characters[i]);
+          console.log(characters[i])
           break;
         default:
           characters[i] = new Knight
-          console.log(characters[i]);
-        }
-      }
-      fight([characters]);
-      function fight([characters]){
-        while (player_one.health > 0 && player_two.health > 0){
-          var random_specs = Math.floor(Math.random() * 10) + 1;
-            console.log(random_specs)
-            //turn = turn == 1 ? 0 : 1
-            if (random_specs >= 9){
-              turn == 0 ? player_one.spower(player_two) : player_two.spower(player_one)
-              console.log("super attack");
-              turn = turn == 1 ? 0 : 1
-            } else {
-              turn == 0 ? player_one.attack(player_two) : player_two.attack(player_one)
-              turn = turn == 1 ? 0 : 1
-            }
-          }
-        // player_two.health = random_specs >= 9 ? player_two.health - player_one.power : player_two.health
-        var winner = player_two.health <= 0 ? "Wins player one " : "Wins player two"
-        console.log(winner); 
+          console.log(characters[i])
       }
     }
+    fight(characters[0], characters[1]);
+    console.log(player_one)
+  }
 
 
 // el primer if es para la equivocacion del ataque
 // el primer else es que no se equivoca
 // despues es el super ataque
+
+
+//do sencuense
+//while (conditin);
